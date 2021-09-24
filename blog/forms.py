2cb9a78176595bf5a36830e969b0ed6373d56eec
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-24 14:15:06 +0800

    collecting data

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
 class SearchForm(forms.Form):
-	q = forms.CharField()
+    q = forms.CharField()
+
+
+class TestForm(forms.Form):
+    some_text = forms.CharField()
+    boolean = forms.BooleanField(required=False)
+    integer = forms.IntegerField()
+    email = forms.EmailField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
+from django.http import request
 from django.shortcuts import render
-from .forms import SearchForm
+from .forms import SearchForm, TestForm
+
 
 def home(req):
-    form = SearchForm()
+    form = TestForm(req.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        print(form.cleaned_data.get('some_text'))
+    # if req.method == 'POST':
+    #    pass
+    # elif req.method == 'GET':
+    #    pass
     return render(req, 'form.html', {'form': form})

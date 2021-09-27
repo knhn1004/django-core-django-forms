6c6f79db6b6868c2031cca54b7cba2358bd440c7
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-27 17:04:14 +0800

    form field label & widgets

--- blog/forms.py
+++ blog/forms.py
@@ -5,10 +5,34 @@ class SearchForm(forms.Form):
     q = forms.CharField()
 
 
+SOME_CHOICES = (
+    ('db-value', 'Display Value'),
+    ('db-value-2', 'Display Value 2'),
+    ('db-value-3', 'Display Value 3'),
+)
+
+INT_CHOICES = [tuple([x, x]) for x in range(0, 100)]
+
+
+YEARS = [x for x in range(1980, 2031)]
+
+
 class TestForm(forms.Form):
-    some_text = forms.CharField()
+    date_field = forms.DateField(
+        widget=forms.SelectDateWidget(years=YEARS), initial="2020-11-20")
+    some_text = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 4,
+            'cols': 10
+        }),
+        label='text'
+    )
+    choices = forms.CharField(
+        widget=forms.Select(choices=SOME_CHOICES)
+    )
     boolean = forms.BooleanField(required=False)
-    integer = forms.IntegerField(initial=100)
+    integer = forms.IntegerField(
+        initial=100, widget=forms.RadioSelect(choices=INT_CHOICES))
     email = forms.EmailField()
 
     def __init__(self, user=None, *args, **kwargs):

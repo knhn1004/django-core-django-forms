2fdbcaf471c53df1859d6500b4d503b601a09112
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-10-04 17:37:36 +0800

    model formset factory

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,7 @@ class Post(models.Model):
     width_field = models.IntegerField(default=0)
     content = models.TextField()
     draft = models.BooleanField(default=False)
-    publish = models.DateField(auto_now=False, auto_now_add=False)
+    publish = models.DateField(auto_now=True, auto_now_add=False, null=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
--- blog/views.py
+++ blog/views.py
@@ -5,20 +5,43 @@ from django.utils import timezone
 from django.forms import formset_factory, modelformset_factory
 
 from .forms import SearchForm, TestForm, PostModelForm
+from .models import Post
 
 
 def formset_view(req):
-    TestFormset = formset_factory(TestForm)
-    formset = TestFormset(req.POST or None)
+    PostModelFormset = modelformset_factory(
+        Post,
+        fields=['user', 'title', 'slug', 'image'],
+        # extra=2
+    )
+    formset = PostModelFormset(req.POST or None)
 
     if formset.is_valid():
+        # formset.save(commit=False)
+        # for form in formset:
+        #    print(form.cleaned_data)
         for form in formset:
-            print(form.cleaned_data)
+            obj = form.save(commit=False)
+            obj.publish = timezone.now()
+            form.save()
 
     context = {
         'formset': formset
     }
     return render(req, 'formset_view.html', context)
+
+# def formset_view(req):
+#    TestFormset = formset_factory(TestForm)
+#    formset = TestFormset(req.POST or None)
+
+#    if formset.is_valid():
+#        for form in formset:
+#            print(form.cleaned_data)
+
+#    context = {
+#        'formset': formset
+#    }
+#    return render(req, 'formset_view.html', context)
 
 
 def home(req):
